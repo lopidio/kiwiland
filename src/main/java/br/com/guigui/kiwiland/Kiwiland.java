f206059fc7416bb62dd763fc52853bb800e62d55
Guilherme Moraes <guilherme.moraes@outlook.com>
2017-03-22 23:46:07 -0300

    Creating eclipse project

---
+++ src/main/java/br/com/guigui/kiwiland/Kiwiland.java
@@ -0,0 +1,13 @@
+package br.com.guigui.kiwiland;
+
+/**
+ * Hello world!
+ *
+ */
+public class Kiwiland 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/br/com/guigui/kiwiland/KiwilandTest.java
@@ -0,0 +1,38 @@
+package br.com.guigui.kiwiland;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple Kiwiland.
+ */
+public class KiwilandTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public KiwilandTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( KiwilandTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testKiwiland()
+    {
+        assertTrue( true );
+    }
+}
