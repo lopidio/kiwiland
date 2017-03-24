f423c69745b256aa5e8124803325e31bbe4e42fa
Guilherme Moraes <guilherme.moraes@outlook.com>
2017-03-23 22:54:26 -0300

    Creating Town and Track

--- src/main/java/br/com/guigui/kiwiland/railroad/Town.java
+++ src/main/java/br/com/guigui/kiwiland/railroad/Town.java
@@ -1,6 +1,33 @@
 package br.com.guigui.kiwiland.railroad;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Town
 {
+	String name;
+	private Map<String, Track> tracks;
+	
+	public Town(String name)
+	{
+		this.name = name;
+		tracks = new HashMap<String, Track>();
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void addTrack(Track track)
+	{
+		tracks.put(track.getDestiny().getName(), track);
+	}
+
+	public Collection<Track> getTracks()
+	{
+		return tracks.values();
+	}
 
 }
--- src/main/java/br/com/guigui/kiwiland/railroad/Track.java
+++ src/main/java/br/com/guigui/kiwiland/railroad/Track.java
@@ -2,5 +2,30 @@ package br.com.guigui.kiwiland.railroad;
 
 public class Track
 {
+	Town origin;
+	Town destiny;
+	int distance;
+
+	public Track(Town origin, Town destiny, int distance)
+	{
+		this.origin = origin;
+		this.destiny = destiny;
+		this.distance = distance;
+	}
+
+	public Town getOrigin()
+	{
+		return origin;
+	}
+
+	public Town getDestiny()
+	{
+		return destiny;
+	}
+
+	public int getDistance()
+	{
+		return distance;
+	}
 
 }
